5f9e2c94f3ebdac45dc00b7ca742c9ab608a13f0
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-07-23 21:08:12 +0700

    Expanded and padding todolist

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class TodoList extends StatefulWidget {
   static const String nameRoute = "/todo_list";
-  const TodoList({Key? key}) : super(key: key);
+
+  TodoList({Key? key}) : super(key: key);
 
   @override
   _TodoListState createState() => _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
+  List<String> _todos = [for (var i = 0; i < 100; i++) 'Todo $i'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +18,35 @@ class _TodoListState extends State<TodoList> {
         title: Text("Todo List"),
       ),
       body: Container(
-        child: Center(
-          child: Text("Todo List"),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 16.0),
+                      child: TextField(),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Add"),
+                    ))
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_todos[index]),
+                  );
+                },
+                itemCount: _todos.length,
+              ),
+            )
+          ],
         ),
       ),
     );
